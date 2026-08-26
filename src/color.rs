2e8495437883f19ab7d6f1a556f84b5fa9c8a6cd
Ada-lave <mega.kefi36@gmail.com>
2026-08-26 20:22:38 +0500

    fix: color fix

--- src/color.rs
+++ src/color.rs
@@ -3,10 +3,10 @@ use embedded_graphics_core::pixelcolor::PixelColor;
 #[repr(u8)]
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub enum WBRYColor {
-    WHITE = 0b00,
-    BLACK = 0b01,
-    RED = 0b10,
-    YELLOW = 0b11
+    BLACK = 0b00,
+    WHITE = 0b01,
+    YELLOW = 0b10,
+    RED = 0b11,
 }
 
 impl PixelColor for WBRYColor {
